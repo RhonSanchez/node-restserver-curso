9dd7bc41187728e1a76ab90b15b527f7527e29ce
Rhon Sanchez <rhonaldsanchez98@gmail.com>
2020-12-25 22:06:23 -0400

    variable de entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -1,5 +1,19 @@
 // =================================
-// Port
+// ENTORNO
 // =================================
 
 process.env.PORT = process.env.PORT || 8080;
+
+// =================================
+// DATA BASE
+// =================================
+
+let urlBD;
+
+if (process.env.NODE_ENV === "dev") {
+    urlBD = "mongodb://localhost:27017/cafe";
+} else {
+    urlBD = process.env.MONGO_URI;
+}
+
+process.env.URLDB;
